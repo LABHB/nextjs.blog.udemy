37c6333e5e5524e8e6a3efa23d372919dcf61312
HACKBERRY <hblab88@gmail.com>
2022-09-20 23:43:52 +0900

    all done

--- next.config.js
+++ next.config.js
@@ -5,3 +5,15 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+// module.exports = {
+//   webpack: (config, { isServer }) => {
+//       // 空のオブジェクト渡すことでnpmパッケージがfsモジュールに依存しないようにします
+//       if (!isServer) {
+//           config.node = {
+//               fs: 'empty'
+//           }
+//       }
+//       return config
+//   }
+// }
